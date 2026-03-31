cb65b370580228a7a2a4f91aab39a6ddeb5eed5b
Dhruv Sonagra <dhruv.sonagra.cg@gmail.com>
2026-03-31 21:44:18 +0530

    Time: 0 ms (100%), Space: 12.3 MB (76.24%) - LeetHub

---
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countElements(vector<int>& nums) {
+       int mn = INT_MAX;
+       int mx = INT_MIN;
+        for(int num : nums){
+            mn = min(mn , num);
+            mx = max(mx , num);
+
+        }
+        int count = 0;
+        for(int num : nums){
+            if(num > mn && num < mx){
+                count++;
+            }
+        }
+        return count;
+    }
+};
